be048d3250c8918e7b3c397e61e36e07977d8ead
Ramón Salazar <el_mejorplanet@hotmail.com>
2023-03-28 16:01:57 -0400

    1.0

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-import express from 'express'
-import http from 'http'
-import cors from 'cors'
-
 const express = require("express");
-const apiRoute = require("./routes/routes");
+const apiRoute= require("./routes/routes");
+
 const app = express();
-const server = http.createServer(app)
+
 const PORT = process.env.PORT || 3000;
-app.use(cors())
 
 app.use(express.json());
 
 app.use("/whatsapp", apiRoute);
 
-server.listen(PORT, () => (console.log("El puerto es: " + PORT)))
+app.listen(PORT, () => (console.log("El puerto es: " + PORT)))
