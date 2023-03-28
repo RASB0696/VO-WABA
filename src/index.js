cc6ee98afab856f46747dfdb3dafeece0c67994d
Ramón Salazar <el_mejorplanet@hotmail.com>
2023-03-28 14:54:04 -0400

    1.2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
+
 const express = require("express");
 const apiRoute = require("./routes/routes");
-
 const app = express();
-
+const server = http.createServer(app)
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
