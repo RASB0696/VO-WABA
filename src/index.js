a98fa2d725272e145a2e6ede4a697e22bf23a78c
Ramón Salazar <el_mejorplanet@hotmail.com>
2023-03-28 15:07:11 -0400

    1.3

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ const apiRoute = require("./routes/routes");
 const app = express();
 const server = http.createServer(app)
 const PORT = process.env.PORT || 3000;
+app.use(cors())
 
 app.use(express.json());
 
 app.use("/whatsapp", apiRoute);
 
-app.listen(PORT, () => (console.log("El puerto es: " + PORT)))
+server.listen(PORT, () => (console.log("El puerto es: " + PORT)))
